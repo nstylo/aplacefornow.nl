ff0d3ff0778c3845334b9d82f8a931e6d852ab50
nstylo <niklas@stylianou.info>
2020-04-22 20:19:59 +0200

    Add ProtectedRoute

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
 import React from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import { ProtectedRoute } from "./Auth/ProtectedRoute"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>A Place For Now boilerplate</p>
-      </header>
-    </div>
-  )
+  return <Router></Router>
 }
 
 export default App
