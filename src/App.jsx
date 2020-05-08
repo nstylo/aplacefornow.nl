a2f1c6eb1ce4f76db04313ae768919f8d7b486d4
nstylo <niklas@stylianou.info>
2020-05-08 19:21:44 +0200

    Add BrandonGrotesque font

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import React from "react"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import { ProtectedRoute } from "./Auth/ProtectedRoute"
+import { ThemeProvider } from "@material-ui/core/styles"
+import CssBaseline from "@material-ui/core/CssBaseline"
+
+// theme
+import { theme } from "./theme"
 
 // import pages
 import About from "./Pages/About"
@@ -13,26 +18,29 @@ import NoMatch from "./Pages/404.jsx"
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/signup">
-          <Signup />
-        </Route>
-        <Route path="/reset-password">
-          <ResetPassword />
-        </Route>
-        <Route path="/forgot-password">
-          <ForgotPassword />
-        </Route>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="*">
-          <NoMatch />
-        </Route>
-      </Switch>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path="/reset-password">
+            <ResetPassword />
+          </Route>
+          <Route path="/forgot-password">
+            <ForgotPassword />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </ThemeProvider>
     </Router>
   )
 }
--- src/Pages/404.jsx
+++ src/Pages/404.jsx
@@ -19,6 +19,114 @@ export default () => {
       <div>
         <Link to="/reset-password">Reset Password</Link>
       </div>
+      <p
+        style={{
+          "font-style": "normal",
+          "font-weight": "100",
+          "font-size": "30px",
+        }}
+      >
+        The risque gown makes
+      </p>
+      <p
+        style={{
+          "font-style": "italic",
+          "font-weight": "100",
+          "font-size": "30px",
+        }}
+      >
+        The risque gown makes
+      </p>
+      <p
+        style={{
+          "font-style": "normal",
+          "font-weight": "300",
+          "font-size": "30px",
+        }}
+      >
+        The risque gown makes
+      </p>
+      <p
+        style={{
+          "font-style": "italic",
+          "font-weight": "300",
+          "font-size": "30px",
+        }}
+      >
+        The risque gown makes
+      </p>
+      <p
+        style={{
+          "font-style": "normal",
+          "font-weight": "400",
+          "font-size": "30px",
+        }}
+      >
+        The risque gown makes
+      </p>
+      <p
+        style={{
+          "font-style": "italic",
+          "font-weight": "400",
+          "font-size": "30px",
+        }}
+      >
+        The risque gown makes
+      </p>
+      <p
+        style={{
+          "font-style": "normal",
+          "font-weight": "600",
+          "font-size": "30px",
+        }}
+      >
+        The risque gown makes
+      </p>
+      <p
+        style={{
+          "font-style": "italic",
+          "font-weight": "600",
+          "font-size": "30px",
+        }}
+      >
+        The risque gown makes
+      </p>
+      <p
+        style={{
+          "font-style": "normal",
+          "font-weight": "700",
+          "font-size": "30px",
+        }}
+      >
+        The risque gown makes
+      </p>
+      <p
+        style={{
+          "font-style": "italic",
+          "font-weight": "700",
+          "font-size": "30px",
+        }}
+      >
+        The risque gown makes
+      </p>
+      <p
+        style={{
+          "font-style": "normal",
+          "font-weight": "900",
+          "font-size": "30px",
+        }}
+      >
+        The risque gown makes
+      </p>
+      <p
+        style={{
+          "font-style": "italic",
+          "font-weight": "900",
+          "font-size": "30px",
+        }}
+      >
+        The risque gown makes
+      </p>
     </>
   )
 }
