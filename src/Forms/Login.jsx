524ce139bc8c89f24a00ce7239d9267bd115ca7a
nstylo <niklas@stylianou.info>
2020-05-17 19:29:11 +0200

    Add Visibility feature for password input

--- src/Forms/Login.jsx
+++ src/Forms/Login.jsx
@@ -2,8 +2,18 @@ import React, { useState } from "react"
 import styled from "styled-components"
 
 // material ui components
-import { Typography, TextField } from "@material-ui/core"
+import {
+  Typography,
+  TextField,
+  Input,
+  InputLabel,
+  FormControl,
+  InputAdornment,
+  IconButton,
+} from "@material-ui/core"
 
+// icons
+import { Visibility, VisibilityOff } from "@material-ui/icons"
 export default () => {
   const [isVisible, setVisible] = useState(false)
   const [mail, setMail] = useState("")
@@ -20,12 +30,30 @@ export default () => {
           value={mail}
           onChange={e => setMail(e.target.value)}
         ></TextField>
-        <TextField
-          label="Password"
-          value={password}
-          type={isVisible ? "text" : "password"}
-          onChange={e => setPassWord(e.target.value)}
-        ></TextField>
+        <FormControl variant="outlines">
+          <InputLabel htmlFor="password-form">Password</InputLabel>
+          <Input
+            id="password-form"
+            value={password}
+            type={isVisible ? "text" : "password"}
+            onChange={e => setPassWord(e.target.value)}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={() => setVisible(!isVisible)}
+                  onMouseDown={e => e.preventDefault()}
+                >
+                  {isVisible ? (
+                    <Visibility style={{ width: "28px", height: "28px" }} />
+                  ) : (
+                    <VisibilityOff style={{ width: "28px", height: "28px" }} />
+                  )}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
       </FormBody>
     </FormWrapper>
   )
