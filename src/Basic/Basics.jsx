e7366231d3efb1641e0a2818f1f6296848b8e068
nstylo <niklas@stylianou.info>
2020-07-11 20:29:40 +0200

    Add basic PasswordTextField

--- src/Basic/Basics.jsx
+++ src/Basic/Basics.jsx
@@ -1,8 +1,19 @@
-import React from "react"
+import React, { useState } from "react"
 import { theme } from "../theme"
 import { makeStyles } from "@material-ui/core"
 
-import { Button as IButton } from "@material-ui/core"
+import {
+  FormControl,
+  InputLabel,
+  Input,
+  InputAdornment,
+  FormHelperText,
+  IconButton,
+  Button as IButton,
+} from "@material-ui/core"
+//
+// icons
+import { Visibility, VisibilityOff } from "@material-ui/icons"
 
 const useStyles = makeStyles({
   root: {
@@ -20,5 +31,56 @@ export const Button = ({ children, ...props }) => {
     <IButton className={classes.root} {...props}>
       {children}
     </IButton>
+  )
+}
+
+export const PasswordTextField = ({
+  helperText,
+  id,
+  label,
+  error,
+  ...props
+}) => {
+  const [isVisible, setVisible] = useState(false)
+
+  return (
+    <FormControl variant="outlines">
+      <InputLabel error={error} htmlFor={id}>
+        {label}
+      </InputLabel>
+      <Input
+        id={id}
+        error={error}
+        type={isVisible ? "text" : "password"}
+        {...props}
+        endAdornment={
+          <InputAdornment position="end">
+            <IconButton
+              aria-label="toggle password visibility"
+              onClick={() => setVisible(!isVisible)}
+              onMouseDown={e => e.preventDefault()}
+              style={{ marginBottom: "16px" }}
+            >
+              {isVisible ? (
+                <Visibility
+                  style={{
+                    width: "26px",
+                    height: "26px",
+                  }}
+                />
+              ) : (
+                <VisibilityOff
+                  style={{
+                    width: "26px",
+                    height: "26px",
+                  }}
+                />
+              )}
+            </IconButton>
+          </InputAdornment>
+        }
+      />
+      {error ? <FormHelperText error>{helperText}</FormHelperText> : null}
+    </FormControl>
   )
 }
