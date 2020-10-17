cddb95206b143142dac5a2395cf80935848dc13a
emrecem <emrecem.kaya@gmail.com>
2020-10-17 16:37:13 +0200

    Bugs fixed, final version (except links for pages and social media)

--- src/Misc/Footer.js
+++ src/Misc/Footer.js
@@ -6,54 +6,71 @@ import { breakpoints } from "../theme"
 const Footer = () => {
   return (
     <Container>
-      <FooterSection>
-        <FooterPaper
+      <FooterSection alignItems="stretch">
+        <Paper
           style={{
             backgroundColor: "#fafafa",
             borderRadius: "25px 25px 0 0",
           }}
           elevation={4}
         >
-          <Section xs={12} top={20} spacing={6} item container justify="center">
-            <FooterColumn header="Get in touch">
-              <P>Contact</P>
-              <P>info@apfn.com</P>
-              <P>+31 040 123 4567</P>
-            </FooterColumn>
-            <FooterColumn header="Legal">
-              <FooterLink href="#">
-                {/*links in here*/}
-                <P>Terms and conditions</P>
-              </FooterLink>
-              <FooterLink href="#">
-                {/*links in here*/}
-                <P>Privacy policy</P>
-              </FooterLink>
-            </FooterColumn>
-            <FooterColumn header="Learn more">
-              <FooterLink href="#">
-                {/*links in here*/}
-                <P>How it works</P>
-              </FooterLink>
-              <FooterLink href="#">
-                {/*links in here*/}
-                <P>About Us</P>
-              </FooterLink>
-              <FooterLink href="#">
-                {/*links in here*/}
-                <P>FAQ</P>
-              </FooterLink>
-            </FooterColumn>
-            <FooterColumn header="Social">
-              <P>
-                Follow us on social media for the latest updates of our progress
-              </P>
-              {/*Social Media Links here*/}
-            </FooterColumn>
-            <hr border="none" width="100%" color="D3D3D3" />
-            <P>2020 A Place For Now, All rights reserved</P>
-          </Section>
-        </FooterPaper>
+          <FooterGrid>
+            <Item>
+              <Section
+                xs={12}
+                top={30}
+                spacing={6}
+                item
+                container
+                justify="center"
+              >
+                <FooterColumn header="Get in touch">
+                  <P>Contact</P>
+                  <P>info@apfn.com</P>
+                  <P>+31 040 123 4567</P>
+                </FooterColumn>
+                <FooterColumn header="Legal">
+                  <FooterLink href="#">
+                    {/*links in here*/}
+                    <P>Terms and conditions</P>
+                  </FooterLink>
+                  <FooterLink href="#">
+                    {/*links in here*/}
+                    <P>Privacy policy</P>
+                  </FooterLink>
+                </FooterColumn>
+                <FooterColumn header="Learn more">
+                  <FooterLink href="#">
+                    {/*links in here*/}
+                    <P>How it works</P>
+                  </FooterLink>
+                  <FooterLink href="#">
+                    {/*links in here*/}
+                    <P>About Us</P>
+                  </FooterLink>
+                  <FooterLink href="#">
+                    {/*links in here*/}
+                    <P>FAQ</P>
+                  </FooterLink>
+                </FooterColumn>
+                <FooterColumn header="Social">
+                  <P>
+                    Follow us on social media for the latest updates of our
+                    progress
+                  </P>
+                  {/*Social Media Links here*/}
+                </FooterColumn>
+                <hr border="none" width="100%" color="D3D3D3" />
+                <Grid container item xs={12} justify="center">
+                  <FooterLink href="#">
+                    {/*links in here*/}
+                    ©2020 A Place For Now, All rights reserved
+                  </FooterLink>
+                </Grid>
+              </Section>
+            </Item>
+          </FooterGrid>
+        </Paper>
       </FooterSection>
     </Container>
   )
@@ -61,21 +78,10 @@ const Footer = () => {
 
 const FooterSection = styled(Grid)`
   position: relative;
-
-  padding-top: 50px;
-
-  @media (max-width: ${breakpoints.sm}px) {
-    padding-left: 40px;
-    padding-right: 40px;
-  }
-
-  @media (max-width: ${breakpoints.xxs}px) {
-    padding-left: 20px;
-    padding-right: 20px;
-  }
+  padding-top: 65px;
+  min-height: 300px;
 `
 const Section = styled(Grid)`
-  position: flex;
   padding-left: 80px;
   padding-right: 80px;
   padding-top: ${(props) => props.top}px;
@@ -90,8 +96,6 @@ const Section = styled(Grid)`
     padding-right: 20px;
   }
 `
-//was declared as display: grid but that caused visual bug for some reason
-const FooterPaper = styled(Paper)``
 
 const UFooterColumn = ({ header, children, className }) => {
   return (
@@ -107,15 +111,14 @@ const UFooterColumn = ({ header, children, className }) => {
 const P = styled(Typography).attrs(() => ({
   variant: "body1",
 }))`
-  text-align: justify;
   margin-top: 10px;
   width: auto;
 `
 
 const Container = styled.div`
-  display: grid;
   width: 100%;
-  align-items: center;
+  overflow: hidden;
+  min-height: 300px;
 `
 
 const FooterColumn = styled(UFooterColumn)``
@@ -128,15 +131,20 @@ const FooterLink = styled(Link).attrs(() => ({
   margin-top: 10px;
   width: auto;
 `
-/*
-This doesn't work for some reason when put as a parent to <Section>
-and child to FooterPaper
-/
-/*
+
 const Item = styled.div`
   grid-column-start: 2;
   grid-column-end: 3;
+  overflow: hidden;
 `
-*/
+
+const FooterGrid = styled.div`
+  display: grid;
+  overflow: hidden;
+  min-height: 300px;
+  @media (min-width: ${breakpoints.xl}px) {
+    grid-template-columns: auto ${breakpoints.xl}px auto;
+  }
+`
 
 export default Footer
